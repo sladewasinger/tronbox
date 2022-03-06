7a90d1d0266744c38789c5f57cf214863482faeb
Austin Wasinger <austinwasinger@gmail.com>
2022-03-05 17:48:40 -0700

    Added really dirty code and initial trail algorithm

--- spa/src/components/grid-canvas/grid-canvas.vue
+++ spa/src/components/grid-canvas/grid-canvas.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>The Grid</h3>
+    <button v-on:click="start()">Start (add tron bike)</button>
     <canvas :id="canvasId" class="canvas-style" />
   </div>
 </template>
@@ -13,6 +14,42 @@ import { Point, Size } from "paper/dist/paper-core";
 // packages
 const paper = require("paper");
 
+var MoveDirection = {
+  UP: new Point(0, -1),
+  DOWN: new Point(0, 1),
+  LEFT: new Point(-1, 0),
+  RIGHT: new Point(1, 0),
+  STALL: new Point(0, 0)
+};
+
+function ai_v1_Clockwise(grid, headPos) {
+  var move = MoveDirection.RIGHT;
+  var nextPos = new Point(headPos.x + move.x, headPos.y + move.y);
+  if (headPos.x + 1 >= grid.length || isOccupied(grid, nextPos)) {
+    move = MoveDirection.DOWN;
+    nextPos = new Point(headPos.x + move.x, headPos.y + move.y);
+    if (headPos.y + 1 >= grid[0].length || isOccupied(grid, nextPos)) {
+      move = MoveDirection.LEFT;
+      nextPos = new Point(headPos.x + move.x, headPos.y + move.y);
+      if (headPos.x - 1 < 0 || isOccupied(grid, nextPos)) {
+        move = MoveDirection.UP;
+        nextPos = new Point(headPos.x + move.x, headPos.y + move.y);
+        if (headPos.y - 1 < 0 || isOccupied(grid, nextPos)) {
+          move = MoveDirection.STALL;
+        }
+      }
+    }
+  }
+  return move;
+}
+
+function isOccupied(grid, pos) {
+  if (grid[pos.x][pos.y].occupied) {
+    return true;
+  }
+  return false;
+}
+
 export default {
   name: "GridCanvas",
   props: ["canvasId"],
@@ -23,55 +60,140 @@ export default {
       x: 0,
       y: 0,
     },
-    box: undefined,
+    grid: undefined,
+    trails: [],
+    renderStuff: {},
   }),
   methods: {
     reset() {
       this.scope.project.activeLayer.removeChildren();
     },
-    drawGrid() {
-      var num_rectangles_wide = 10;
-      var num_rectangles_tall = 11;
-      var boundingRect = this.scope.view.bounds;
-      var width_per_rectangle = 50; // boundingRect.width / num_rectangles_wide;
-      var height_per_rectangle = 50; // boundingRect.height / num_rectangles_tall;
-      for (var i = 0; i < num_rectangles_wide; i++) {
-        for (var j = 0; j < num_rectangles_tall; j++) {
-          var aRect = new paper.Path.Rectangle(
-            boundingRect.left + i * width_per_rectangle,
-            boundingRect.top + j * height_per_rectangle,
-            width_per_rectangle,
-            height_per_rectangle
+    createGrid() {
+      this.grid = [];
+      var gridWidth = 10;
+      var cellWidth = 50;
+      for (var x = 0; x < gridWidth; x++) {
+        this.grid[x] = [];
+        for (var y = 0; y < gridWidth; y++) {
+          var renderedCell = new paper.Path.Rectangle(
+            x * cellWidth,
+            y * cellWidth,
+            cellWidth,
+            cellWidth
           );
-          aRect.strokeColor = "#290";
-          aRect.fillColor = "#090909";
+          let fillColor = "#090909";
+          if (
+            (x == 0 || x + 1 == gridWidth) &&
+            (y == 0 || y + 1 == gridWidth)
+          ) {
+            fillColor = "#221";
+          }
+          renderedCell.strokeColor = "#290";
+          renderedCell.fillColor = fillColor;
+
+          var cell = {
+            ownerId: undefined,
+            get occupied() {
+              return !!this.ownerId;
+            }
+          };
+          this.grid[x].push(cell);
         }
       }
     },
-    drawPoint() {
-      this.box = new paper.Path.Rectangle(new Point(0, 0), new Size(50, 50));
-      this.box.fillColor = "#FFFF00";
+    createTrail(x, y) {
+      let trail = {
+        head: new Point(x, y),
+        tail: [], // queue type?
+        color: "#F00",
+        alive: true,
+        ownerId: 1,
+        getMove: ai_v1_Clockwise,
+        applyMove(grid, headPos, moveDir) {
+          if (moveDir == MoveDirection.STALL) {
+            console.log(`${this.ownerId} returned stall`);
+            return;
+          }
+          let move = new Point(headPos.x + moveDir.x, headPos.y + moveDir.y);
+          if (move.x < 0 || move.x >= grid.length) {
+            this.alive = false;
+            return;
+          }
+          if (move.y < 0 || move.y >= grid[0].length) {
+            this.alive = false;
+            return;
+          }
+          grid.forEach((col, x) => {
+            col.forEach((cell, y) => {
+              if (move.x == x && move.y == y && cell.occupied) {
+                this.alive = false;
+                console.log(`${this.ownerId} hit other snake at: (" + x + ", " + y + ")`);
+                return;
+              }
+            });
+          });
+          this.tail.push(this.head);
+          this.head = move;
+        },
+      };
+      return trail;
+    },
+    start() {
+      var trail = this.createTrail(0, 0);
+      this.trails.push(trail);
+      this.grid[trail.head.x][trail.head.y].ownerId = trail.ownerId;
+      this.gameLoop();
+    },
+    render() {
+      if (this.renderStuff.grid === undefined) {
+        var gridWidth = 10;
+        var cellWidth = 50;
+
+        this.renderStuff.grid = this.grid.map((col, x) =>
+          col.map((el, y) => {
+            var cell = new paper.Path.Rectangle(
+              x * cellWidth,
+              y * cellWidth,
+              cellWidth,
+              cellWidth
+            );
+            cell.strokeColor = "#290";
+            let fillColor = "#090909";
+            if (
+              (x == 0 || x + 1 == gridWidth) &&
+              (y == 0 || y + 1 == gridWidth)
+            ) {
+              fillColor = "#222";
+            }
+            cell.fillColor = fillColor;
+            return cell;
+          })
+        );
+      }
+      for (let trail of this.trails) {
+        for (let point of [...trail.tail, trail.head]) {
+          this.renderStuff.grid[point.x][point.y].fillColor = trail.color;
+        }
+      }
+    },
+    iterateTrails() {
+      for (let trail of this.trails.filter(x => x.alive)) {
+        var moveDir = trail.getMove(this.grid, trail.head);
+        trail.applyMove(this.grid, trail.head, moveDir);
+        this.grid[trail.head.x][trail.head.y].ownerId = trail.ownerId;
+      }
+    },
+    gameLoop() {
+      this.iterateTrails();
+      this.render();
+      setTimeout(this.gameLoop.bind(this), 50);
     },
   },
   mounted() {
     this.scope = new paper.PaperScope();
     this.scope.setup(this.canvasId);
-    this.drawGrid();
-    this.drawPoint();
-    setInterval(() => {
-      if (!this.box) {
-        console.log("box undefined!");
-        return;
-      }
-      this.box.position.x += 10;
-      this.box.position.y += 10;
-      if (this.box.position.x > 500) {
-        this.box.position.x = 0;
-      }
-      if (this.box.position.y > 500) {
-        this.box.position.y = 0;
-      }
-    }, 50);
+    this.createGrid();
+    this.start();
   },
 };
 </script>
---
+++ vim-cheatsheet.md
@@ -0,0 +1,46 @@
+# Scroll Up/Down
+
+Ctrl-y Moves screen up one line  
+Ctrl-e Moves screen down one line  
+Ctrl-u Moves cursor & screen up ½ page  
+Ctrl-d Moves cursor & screen down ½ page  
+Ctrl-b Moves screen up one page, cursor to last line  
+Ctrl-f Moves screen down one page, cursor to first line
+
+# Delete commands
+
+`diw` - Delete the entire word under the cursor  
+`daw` - Same as diw but including trailing whitespace  
+`dw` deletes to end of word starting at cursor  
+`dd` deletes entire line  
+`ci(` will delete everything within parenthesis (change in parenthesis)
+
+# Move cursor up/down
+
+10j move down 10 lines  
+10k move up 10 lines
+
+# Move cursor to end of line
+
+\$ moves cursor to end of line  
+\^ moves to start of line
+
+# Move cursor to start of word
+
+b moves cursor to previous word  
+e moves cursor to end of word
+w moves cursor to next word
+
+# Re-center cursor (redraw screen)
+
+zz redraws screen centered at cursor
+
+# Copy/Paste
+
+p to paste
+d to (cut) line
+
+# Insert New Line
+
+o to insert new line below
+O to insert new line above cursor
