a1ecdb3e012e78ab626de001135f2b0d7b4ae897
Austin Wasinger <austinwasinger@gmail.com>
2022-03-07 22:13:36 -0700

    Added better images

--- spa/vue.config.js
+++ spa/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/tronbox/',
+  outputDir: '../docs',
   devServer: {
     proxy: {
       '^/api': {
