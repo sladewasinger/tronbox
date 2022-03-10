a24831e90de834a5f6be808818be1de2105b5834
Austin Wasinger <austinwasinger@gmail.com>
2022-03-09 19:34:57 -0700

    before adding bootstrap

--- spa/src/components/tron-box/tron-box.vue
+++ spa/src/components/tron-box/tron-box.vue
@@ -15,6 +15,17 @@
           <button class="btn btn-dark" @click="togglePause()">{{ pauseBtnText }}</button>
         </div>
         <textarea v-model="aiJs" class="code-area"></textarea>
+        <b-tabs content-class="mt-3">
+          <b-tab title="First" active>
+            <p>I'm the first tab</p>
+          </b-tab>
+          <b-tab title="Second">
+            <p>I'm the second tab</p>
+          </b-tab>
+          <b-tab title="Disabled" disabled>
+            <p>I'm a disabled tab!</p>
+          </b-tab>
+        </b-tabs>
       </div>
     </div>
   </div>
--- spa/src/main.js
+++ spa/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import 'bootstrap'
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/index.scss';
 
