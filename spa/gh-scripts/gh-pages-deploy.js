2bc6eb9d33430d2289884b3da0294b1504e42cda
Austin Wasinger <austinwasinger@gmail.com>
2022-03-14 21:26:22 -0600

    updated deploy js to use import

--- spa/gh-scripts/gh-pages-deploy.js
+++ spa/gh-scripts/gh-pages-deploy.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import execa from "execa";
+import { existsSync } from "fs";
 (async () => {
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         console.log("Building started...");
         await execa("npm", ["run", "build"]);
-        const folderName = fs.existsSync("dist") ? "dist" : "build";
+        const folderName = existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
